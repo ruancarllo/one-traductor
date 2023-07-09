763f83743269fdebee1d76107176bca66d8cbe9d
ruancarllo <ruan4ks@gmail.com>
2023-07-09 13:44:16 -0300

    Changed project identation for two spaces

--- run.js
+++ run.js
@@ -4,11 +4,11 @@ const fs = require('fs');
 var port = 3030;
 
 let server = http.createServer((request, response) => {
-	fs.readFile(__dirname + '/app.html', (error, data) => {
-		response.writeHead(200, {'Content-Type': 'text/html'});
-		response.write(data);
-		response.end();
-	});
+  fs.readFile(__dirname + '/app.html', (error, data) => {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(data);
+    response.end();
+  });
 });
 
 server.listen(port, () => console.log(`App running at http://localhost:${port}`));
